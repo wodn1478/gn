d294b8f2895b2e6ef95b10155df6feb982ca9f3e
wodn1478 <39799541+wodn1478@users.noreply.github.com>
2021-03-14 18:19:35 +0900

    Update gnb.py

--- gnb.py
+++ gnb.py
@@ -1,69 +1,99 @@
-  
+
 import discord
+import requests
+from bs4 import BeautifulSoup
 from discord.ext import commands
-import asyncio
-
-import os
-
-client = discord.Client()
-bot = commands.Bot(command_prefix='T')
-
+bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
-    print("로그인")
-    print(bot.user.name)
-    print(bot.user.id)
-    print("------------------")
-    await bot.change_presence(game=discord.Game(name="서버주소: gnnetwork.kr", type=1))
-@bot.event
-async def on_member_join(member):
-    role = ""
-    for k in member.server.roles:
-        if k.name == "User":
-            role = k
-            break
-    await bot.add_roles(member, role)
-    embed = discord.Embed(title="RPSV 관리자", description="알피서버 사전예약 이벤트중! 지금 당장 신청하자\n\n1.5.2 최강자를 가려라 약한사람은 필요없다 -RP-\n\n1주차 최강자전 보상 : 서버아이템 ( 미정 )\n2주차 최강자전 보상 : 서버아이템 ( 미정 )+ 문화상품권 2만원\n3주차 최강자전 보상 : 서버아이템 ( 미정 )\n4주차 최강자전 보상 : 문화상품권 5만원+서버아이템 ( 미정 )", color=0x00ff00)
-    await bot.send_message(bot.get_channel("557527899993800716"), embed=embed)
+    game = discord.Game("TESTING")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+    print("READY")
 @bot.event
 async def on_message(message):
-    if message.content.startswith("/상담"):
-        id = message.author.id
-        server = message.server
-        name = message.author.name
-        name2 = name.lower()
-        everyone = discord.PermissionOverwrite(read_messages=False, send_messages=False, create_instant_invite=False,
-                                                           manage_channel=False, manage_permissions=False, manage_webhooks=False,
-                                                           send_TTS_messages=False, manage_messages=False, embed_links=False,
-                                                           attach_files=False, read_message_history=False, mention_everyone=False,
-                                                           use_external_emojis=False, add_reactions=False)
-        Member = discord.PermissionOverwrite(read_messages=True, send_messages=True, create_instant_invite=False,read_message_history=True,
-                                                         manage_channel=False, manage_permissions=False, manage_webhooks=False)
-        member_perms = [(mentioned, Member) for mentioned in message.mentions]
-        await bot.create_channel(server, name2, (discord.utils.get(message.server.roles, name="@everyone"), everyone),
-                                                (discord.utils.get(message.server.members, name=name), Member),
-                                             *member_perms, type=discord.ChannelType.text)
-        
-        
-        await bot.send_message(discord.utils.get(message.server.channels, name=name2), "@everyone\n"+ "<@"+id+">님의 상담을 시작합니다.\n상담 내용은 본인, 관리진만 확인 할 수 있습니다.\n상담이 끝나면 채널이 삭제 될 수도 있습니다.")
-        
-        
-    if message.content.startswith("공지띄우기1"):
-        await bot.delete_message(message)
-        embed = discord.Embed(title="RPSV 관리자",
-                              description="알피서버 사전예약 이벤트중! 지금 당장 신청하자\n\n1.5.2 최강자를 가려라 약한사람은 필요없다 -RP-\n\n1주차 최강자전 보상 : 서버아이템 ( 미정 )\n2주차 최강자전 보상 : 서버아이템 ( 미정 )+ 문화상품권 2만원\n3주차 최강자전 보상 : 서버아이템 ( 미정 )\n4주차 최강자전 보상 : 문화상품권 5만원+서버아이템 ( 미정 )",
-                              color=0x00ff00)
-        await bot.send_message(message.channel, embed=embed)
-    if message.content.startswith("/서버정보"):
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="서버 이름", value="RP서버", inline=True)
-        embed.add_field(name="서버 관리자", value="미즈, 다솔, 독사", inline=True)
-        embed.add_field(name="서버 주소", value="RPSV.kr", inline=True)
-        embed.add_field(name="오픈일", value="2019-03-23", inline=True)
-        embed.add_field(name="카페 주소", value="https://cafe.naver.com/rpsv", inline=True)
-        embed.set_thumbnail(url=message.author.avatar_url)
-        await bot.send_message(message.channel, embed=embed)
-        
+    if message.author.bot:
+        return None
+    if message.content[0:4] == "$검색 ":
+        messagesplit = message.content.split(" ")
+        messagesplit = ''.join(messagesplit[1:])
+        webpage = requests.get("https://www.google.com/search?q="+str(messagesplit)+"&hl=ko&tbm=isch")
+        soup = BeautifulSoup(webpage.text, "html.parser")
+        img = soup.find_all('img')
+        img2=img[2]
+        img_src=img2.get('src')
+        embed = discord.Embed(title=message.content[4:]+":",
+                              description="https://www.google.com/search?q="+str(messagesplit)+"&hl=ko&tbm=isch",
+                              color=0x383b38)
+        embed.set_footer(
+            icon_url="https://www.google.com/search?q="+str(messagesplit)+"&hl=ko&tbm=isch")
+        embed.set_image(url=img_src)
+        await message.channel.send(message.content, embed=embed)
+    if message.content[0:4] == "$계산 ":
+        splitphase = message.content.split(" ")
+        first1 = int(splitphase[1])
+        second2 = splitphase[2]
+        third3 = int(splitphase[3])
+        result2 = 0
+        if second2 == "*":
+            result2 = first1 * third3
+        elif second2 == "/":
+            result2 = first1 / third3
+        elif second2 == "+":
+            result2 = first1 + third3
+        elif second2 == "-":
+            result2 = first1 - third3
+        await message.channel.send(result2)
+    if message.content[0:5] == "$계산기 ":
+        splitphase = message.content.split(" ")
+        result3 = int(splitphase[1])
+
+        for i in range(len(splitphase)):
+            if i == 0 or i == 1:
+                pass
+            elif splitphase[i] != "*" and splitphase[i]!="+"and splitphase[i]!= "/"and splitphase[i]!= "-":
+                pass
+            elif splitphase[i] == "*":
+                result3 *= int(splitphase[i+1])
+            elif splitphase[i] == "+":
+                result3 += int(splitphase[i+1])
+            elif splitphase[i] == "-":
+                result3 -= int(splitphase[i+1])
+            elif splitphase[i] == "/":
+                result3 /= int(splitphase[i+1])
+        await message.channel.send(result3)
+    if message.content[0:4] == "$삭제 ":
+        split4 = message.content.split(" ")
+        split4 = int(split4[1])
+        await message.channel.purge(limit=split4)
+    if message.content[0:4] == "$도배 ":
+        split5 = message.content.split(" ")
+        split6 = int(split5[1])
+        split7 = ' '.join(split5[2:])
+        for i in range(split6):
+            await message.channel.send(split7)
+    if message.content[0:4] == "$번역 ":
+        url2 = 'https://papago.naver.com/'
+        q = message.content[4:]
+
+        data2 = {'source': 'en',
+                'target': 'ko',
+                 'text': ''}
+        data2['text'] = q
+
+
+        html = requests.post('https://papago.naver.com/', data=data2).text
+        response = json.loads(html)
+        dict = response['message']['result']['translatedText']
+
+        await message.channel.send(q)
+
+        await message.channel.send(dict)
+
+
+
+
+
+
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
